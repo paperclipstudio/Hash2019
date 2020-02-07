b74e3d8702dcd412f21e2a15012fdb3c7f2626b2
Gus Rendle <gus.rendle@Protonmail.com>
2020-02-07 00:31:39 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,15 +2,10 @@ import java.io.FileNotFoundException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		String a = "src/inputFiles/a_example.txt";
-		try {
-			Photo[] photos = Parser.parseRidesFromFile(a);
-			System.out.println(photos[0].id);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Photo[] photos = Parser.parseRidesFromFile(a);
+		System.out.println(photos[0].id);
 	}
 
 }
