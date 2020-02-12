49b4e1037597ae1fe0d116c3e605bb0058985fa3
Yannki <60620075+Yannki@users.noreply.github.com>
2020-02-12 19:43:33 +0100

    Update Slide.java

--- Slide.java
+++ Slide.java
@@ -1,37 +1,49 @@
+import java.util.ArrayList;
+
 /**
  * @author Lucy
- * @version 1.0
+ * @version 2.0 upgraded by Yan Yan Ji
  */
 public class Slide {
 
-private final Photo pic1;
-private final Photo pic2;
-private final String[] tags;
+	private final Photo pic1;
+	private final Photo pic2;
+	private final String[] tags;
 
-public Slide(Photo newPic1) {
-    pic1 = newPic1;
-    pic2 = null;
-    addTagsToArray1();
-}
+	public Slide(Photo newPic1) {
+		pic1 = newPic1;
+		pic2 = null;
+		tags = newPic1.tags;
+	}
 
-public Slide(Photo newPic1, Photo newPic2) {
-    pic1 = newPic1;
-    pic2 = newPic2;
-    addTagsToArray2();
-}
+	public Slide(Photo newPic1, Photo newPic2) {
+		pic1 = newPic1;
+		pic2 = newPic2;
+		tags = MergeTags(newPic1, newPic2);
+	}
 
-// Lucy needs to fix this so only one addTagsEct
-private void addTagsToArray1() {
-    Set newtags = protected HashSet();
-    newtags.addAll(pic1.getTags());
-    tags = newtags.toArray();
-}
+	private String[] MergeTags(Photo newPic1, Photo newPic2) {
+		ArrayList<String> tagList = new ArrayList<String>();
+		for (String tag : newPic1.tags) {
+			tagList.add(tag);
+		}
+		for (String tag : newPic2.tags) {
+			if (!tagList.contains(tag)) {
+				tagList.add(tag);
+			}
+		}
+		String[] tags = new String[tagList.size()];
+		for (int i = 0; i < tagList.size(); i++) {
+			tags[i] = tagList.get(i);
+		}
+		return tags;
+	}
 
-private void addTagsToArray2() {
-    Set newtags = protected HashSet();
-    newtags.addAll(pic1.getTags());
-    newtags.addAll(pic2.getTags());
-    tags = newtags.toArray();
-}
-
+	public String toString() {
+		if (pic2 == null) {
+			return pic1.id + "\n";
+		} else {
+			return pic1.id + " " + pic2.id + "\n";
+		}
+	}
 }
