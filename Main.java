5a1a05ef8724248a59dbb97c2260fa336f3283ec
Yannki <60620075+Yannki@users.noreply.github.com>
2020-02-12 18:46:26 +0000

    Yan Yan Ji main

--- Main.java
+++ Main.java
@@ -1,11 +1,37 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException {
-		String a = "src/inputFiles/a_example.txt";
+	public static void main(String[] args) throws IOException {
+		String a = "C:\\Users\\yanya\\eclipse-workspace\\Hash2019\\Hash2019-master\\src\\inputFiles\\e_shiny_selfies.txt";
 		Photo[] photos = Parser.parsePhotosFromFile(a);
-		System.out.println(photos[0].id);
+		Comparator<Photo> byAmountOfTags = Comparator.comparing(Photo :: getAmountOfTags);
+		Arrays.sort(photos, byAmountOfTags);
+		ArrayList<Slide> slides = new ArrayList<>();
+		for (int i = 0; i < photos.length; i++) {
+			if (photos[i] != null) {
+				if (photos[i].isVertical) {
+					for (int j = i + 1; j < photos.length; j++) {
+						if (photos[j].isVertical) {
+							slides.add(new Slide(photos[i], photos[j]));
+							photos[j] = null;
+							break;
+						}
+					}
+				}else {
+					slides.add(new Slide(photos[i]));
+				}
+			}
+		}
+		Parser.parseSlidesToFile(slides, "C:\\Users\\yanya\\eclipse-workspace\\Hash2019\\Hash2019-master\\src\\inputFiles\\output.txt");
+		for (Slide s: slides) {
+			System.out.print(s);
+		}
 	}
 
 }
